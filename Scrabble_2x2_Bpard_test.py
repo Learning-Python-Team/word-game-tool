84ec29201c0c7eae46dd85ee889faa3dc80f98d3
MrDayKwan <hollandasa@gmail.com>
2019-08-15 17:51:56 +0100

    Update Scrabble_2x2_Bpard_test.py
    added the function of word to value

--- Scrabble_2x2_Bpard_test.py
+++ Scrabble_2x2_Bpard_test.py
@@ -1,11 +1,13 @@
+
+
 # Values of individual alphabet tiles
 
-tiles_points = {'c' : 3, ' d' : 2}
+tiles_points = {'c': 3, ' d': 2}
 
 
 # Quantity of individual alphabet tiles. Probably wont be used.
 
-tiles_qty = {'a' : 9, 'b' : 2}
+tiles_qty = {'a': 9, 'b': 2}
 
 
 # Simple 2x2 board
@@ -14,9 +16,9 @@ tiles_qty = {'a' : 9, 'b' : 2}
 # Values: board position values
 # e.g. dw = double word score, tl = triple letter score
 
-dict_board = {'a1' : 'dw', 'a2' :'dw', 'b1' : 'tl', 'b2': 'tl'}
-list_board_dw = ['a1' , 'a2']
-list_board_tl = ['b1' , 'b2']
+dict_board = {'a1': 'dw', 'a2': 'dw', 'b1': 'tl', 'b2': 'tl'}
+list_board_dw = ['a1', 'a2']
+list_board_tl = ['b1', 'b2']
 
 
 # For testing - valid input is ab or ba
@@ -31,8 +33,7 @@ word_start = str(input())
 
 
 
-# This tests if input for word_start is a valid board location. 
-
+# This tests if input for word_start is a valid board location.
 if word_start not in list(board.keys()):
     print('Sorry, invalid input. Try again? Yes or No')
 else:
@@ -50,6 +51,41 @@ word_list = list(word)
 for letter in word_list:
     if letter in tiles_points.keys():
         print(tiles_points.get(letter))
-    else:
-       
-    
+
+
+# define function to convert a word to the point value of its' tiles
+def point_conversion(word):
+    # dictionary of letters and their point values
+    tile_value_dict = {
+        ' ': 0,
+        'A': 1,
+        'B': 3,
+        'C': 3,
+        'D': 2,
+        'E': 1,
+        'F': 4,
+        'G': 2,
+        'H': 4,
+        'I': 1,
+        'J': 8,
+        'K': 5,
+        'L': 1,
+        'M': 3,
+        'N': 1,
+        'O': 1,
+        'P': 3,
+        'Q': 10,
+        'R': 1,
+        'S': 1,
+        'T': 1,
+        'U': 1,
+        'V': 4,
+        'W': 4,
+        'X': 8,
+        'Y': 4,
+        'Z': 10,
+    }
+    score = 0
+    for char in word:
+        score += tile_value_dict[char]
+    return score
