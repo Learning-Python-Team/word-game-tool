148a9c3648e88f232f9958b874b18c822b5fb77f
Ahmed221a <al-ghamdi.221@live.com>
2019-08-15 15:49:46 +0300

    first push to start us off

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # scrabble-tool
 Repo for those participating in the scrabble tool project
+
+
+
+#Scrabble Score Tool
+The Project details taken from the group's excel sheet.
+Credit for this project idea goes to Jo.
+
+
+
+# Project details
+"Very basic tool, ideal for complete beginners.
+I see this project as building one part on top of the other, not individual programs. This will allow beginners to consider building a basic menu of which part a user wants to do.
+
+I have done part 1 and 2 in python before so I know roughly what we need to do, but I wanted something even beginners can help build. So, I'm starting from scratch with this and we can see what we come up with together. Then as everyone progresses we can look at part 3 and 4 which I have never done before and thought it would be good to tackle together.
+
+The 4 parts
+
+Part 1: Score Finder - user enters their word and it calculates the score in scrabble
+
+Part 2: Score Compare - user gives 2 words and you indicate which is the better scorer
+
+Part 3: Word Finder - user gives their letters and tool shows them the best scoring word (or top 5 words) they can make with them
+
+Part 4: Build a user interface for the tool"
+
+
+
+# General plan
+
+part 1:
+1. Create a game board on which the player (user) can place their letters
+2. use the letter multiplier and letter score to calculate the base score of the word
+3. use the word multiplier to calculate the total score of the word
---
+++ scrabble_main.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+def buildBoard():
+    # to build the board we create multiple layers:
+    # 1. a user input layer which will be updated later to carry the input word
+    # 2. a letter multiplier layer that will be used to calculate the word score
+    # 3. a word multiplier layer that will be used to calculate the total word score
+    # the layers will be a 15x15 2d numpy array, which will be stacked and returned to the main function
+
+    # define locations of triple and double word tiles (taken from an actual board)
+    triple_word_idx = ((0, 0, 0, 7, 7, 14, 14, 14), (0, 7, 14, 0, 14, 0, 7, 14))
+    double_word_idx = ((1, 1, 2, 2, 3, 3, 4, 4, 10, 10, 11, 11, 12, 12, 13, 13,7),
+                       (1, 13, 2, 12, 3, 11, 4, 10, 4, 10, 3, 11, 2, 12, 1, 13,7))
+    # create the word multiplier layer
+    word_multiplier_array = np.full((15, 15), 1)
+    word_multiplier_array[triple_word_idx] = 3
+    word_multiplier_array[double_word_idx] = 2
+
+    # the same is done for the letter multiplier layer
+    double_letter_idx = ((0, 0, 2, 2, 3, 3, 3, 6, 6, 6, 6, 7, 7, 8, 8, 8, 8, 11, 11, 11, 12, 12, 14, 14),
+                         (3, 11, 6, 8, 0, 7, 14, 2, 6, 8, 12, 3, 11, 2, 6, 8, 12, 0, 7, 14, 6, 8, 3, 11))
+    triple_letter_idx = ((1, 1, 5, 5, 5, 5, 9, 9, 9, 9, 13, 13),
+                         (5, 9, 1, 5, 9, 13, 1, 5, 9, 13, 5, 9))
+    letter_multiplier_array = np.full((15, 15), 1)
+    letter_multiplier_array[double_letter_idx] = 2
+    letter_multiplier_array[triple_letter_idx] = 3
+
+    # create the input layer
+    input_array = np.full((15, 15), np.nan)
+
+    # finally combine the three layers to a 3d array and return
+    return np.stack((input_array, letter_multiplier_array, word_multiplier_array))
+
+
+def userInput():
+    pass
+    # input first letter tile
+    # input direction
+    # input number of letter
+    # update the input array of the game board
+
+
+def calculateScore():
+    pass
+    # this will be used to calculate the user score based on their input and the game board score modifiers
+
+
+if __name__ == "__main__":
+    # build the game board
+    board = buildBoard()
+
+    print("user input\n",board[0])
+
+    # update the game board using user input
+    userInput()
+
+    # calculate score
+    calculateScore()
