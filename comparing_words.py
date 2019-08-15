7031ff6dc192f755db5056c545badf5e72678bb6
shyamcody <46677829+shyamcody@users.noreply.github.com>
2019-08-16 03:11:41 +0530

    comparing_words.py
    This uses the point_conversion function and provides the best words from a list of words

--- comparing_words.py
+++ comparing_words.py
@@ -1,9 +1,14 @@
+#please edit this below import, this may be wrong
+from .word_to_score import point_conversion
+
 #this does not consider the premium squares 
 #the following function finds and returns the highest scoring word from a list of words
 def compare_words(list_of_words):
     scores=[]
     for word in list_of_words:
-        scores.append(word_to_score(word))
+        scores.append(point_conversion(word))
     index_best_word=scores.index(max(scores))
     return list_of_words[index_best_word]
-        
+
+list_of_words=['mom','dad','Queue']
+print(compare_words(list_of_words))
