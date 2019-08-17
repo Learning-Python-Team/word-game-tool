ee3afa70abb15b10f362639ab12d2d88e4b76682
shyamcody <46677829+shyamcody@users.noreply.github.com>
2019-08-17 23:58:03 +0530

    checkstringer.py

---
+++ checkstringer.py
@@ -0,0 +1,10 @@
+
+import re
+character_regex=re.compile('[a-zA-Z]')
+def check_diff_characters(user_input):
+    extra_characters=character_regex.sub(r'',user_input)
+    if len(extra_characters)==0:
+        return False
+    else:
+        return True
+
