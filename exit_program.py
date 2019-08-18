71fd1f9d08846520c97ead29ea3e42441a1fa399
saiyencoder <31190280+saiyencoder@users.noreply.github.com>
2019-08-18 14:18:20 -0500

    Refactor/tidy exit_program.py
    - Removed `upper()` method from condition statements
    - Added `upper()` to the end of `validate` to make it more clean

--- exit_program.py
+++ exit_program.py
@@ -3,18 +3,18 @@ import sys
 
 def exit_program(word):
     # Asks user if they really want to quit the tool
-    validate = input('Are you sure you want to exit the tool (Y)es/(N)o: ')
+    validate = input('Are you sure you want to exit the tool (Y)es/(N)o: ').upper()
 
     # Make sure they enter a valid response to the question, if not, keep asking
-    while validate.upper() != 'Y' and validate.upper() != 'N':
+    while validate != 'Y' and validate != 'N':
         print('\nSorry, that was invalid')
-        validate = input('Please try again. Would you like to exit the tool: ')
+        validate = input('Please try again. Would you like to exit the tool: ').upper()
 
-    # If they enter 'y' or 'Y', tool closes
-    if validate.upper() == 'Y':
+    # If they enter 'y', tool closes
+    if validate == 'Y':
         print('You have exited the tool. Thanks!\n')
         sys.exit()
-    # If they enter 'n' or 'N', goes back to tool
+    # If they enter 'n', goes back to tool
     else:
         print('Alright, back to the tool we go.\n')
         return False
