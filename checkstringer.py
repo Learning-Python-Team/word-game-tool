ae8496b538a6e5cb7dd38fda71bf7d83a8072aec
shyamcody <46677829+shyamcody@users.noreply.github.com>
2019-08-22 17:10:06 +0530

    Delete checkstringer.py

--- checkstringer.py
+++
@@ -1,8 +0,0 @@
-import re
-character_regex=re.compile('[a-zA-Z]')
-def check_diff_characters(user_input):
-    extra_characters=character_regex.sub(r'',user_input)
-    if len(extra_characters)==0:
-        return False
-    else:
-        return True
